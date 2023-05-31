a530107692be8708db51a9c557f5ca8589d619e3
Tatsuya28 <ioiodauphin880@gmail.com>
2023-05-31 15:04:23 +0200

    Update User.ts
    ajout de getter pour l'utilisateur

--- src/classes/User.ts
+++ src/classes/User.ts
@@ -7,4 +7,28 @@ export class User {
               public email : string,
               public reg_date : Date,
               public address : Address) {}
+
+  get userNickname() : string {
+    return this.nickname;
+  }
+
+  get userFirst_name() : string {
+    return this.first_name;
+  }
+
+  get userLast_name() : string {
+    return this.last_name;
+  }
+
+  get userEmail() : string {
+    return this.email;
+  }
+
+  get userReg_date() : Date {
+    return this.reg_date;
+  }
+
+  get userAddress() : Address {
+    return this.address
+  }
 }
