c2ffae2ace6b68b885820b3b30fc74887fa43b13
FRIDBLATT Thomas <thomas.fridblatt@gmail.com>
2023-06-01 09:57:55 +0200

    user ( et modification des informations)

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component,OnInit,Input } from '@angular/core';
+import { UserService } from 'src/app/service/user.service';
 import { User } from 'src/classes/User';
+import { FormGroup,Validators,FormBuilder, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -9,12 +12,40 @@ import { User } from 'src/classes/User';
 export class UserComponent implements OnInit {
   @Input() user!: User;
 
-  constructor(){}
+ password : string = ''
+  confirmpassword: string ='';
+
+  constructor(public service : UserService, private router: Router){
+    this.user = this.service.getuserByIndex(0)
+
+    
+
+
+  }
 
   ngOnInit(): void {
     
   }
-  
-} {
+ onSubmit(): void{
 
-}
+    if(this.password == this.confirmpassword && this.password!="" && this.confirmpassword!="" &&this.user.first_name!="" && this.user.last_name!="" && this.user.email!="" && this.user.nickname!="")
+    {
+      this.user.password=this.password
+      this.service.modifyuser(this.user);
+      this.router.navigate(['/profil']);
+    }
+    else{
+    if (this.user.first_name!="" && this.user.last_name!="" && this.user.email!="" && this.user.nickname!="")
+      {
+        this.service.modifyuser(this.user);
+        this.router.navigate(['/profil']);
+
+    }
+    else{
+
+    }
+  }
+
+ }
+  
+} 
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { Address } from 'src/classes/Address';
 import { User } from 'src/classes/User';
 
@@ -13,14 +14,32 @@ export class UserService {
 
   getUsers():User[]{
     this.arrayuser.push(new User(
+        0,
         "Thomas",
         "Thomas",
         "FRIDBLATT",
         "thomas.fridblatt@gmail.com",
         new Date("07/11/2017"), 
-        new Address(66,"rue du chalet","reims","51100"))
+        new Address(66,"rue du chalet","reims","51100"),
+        "testmdp")
         )
 
     return this.arrayuser;
   }
+
+
+  modifyuser(user: User ): void {
+    console.log("the new first name is ",user.first_name);
+    console.log("the new last name is ",user.last_name);
+    console.log("the new email is ",user.email);
+    console.log("the new nickname is ",user.nickname);
+    console.log("the new mdp is",user.password)
+
+
+  }
+
+  getuserByIndex(id : number): User{
+
+    return this.arrayuser[id]
+   }
 }
