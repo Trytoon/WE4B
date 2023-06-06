60d4208fe8218c9a3857fa4bd86c02693b7a0c8b
FRIDBLATT Thomas <thomas.fridblatt@gmail.com>
2023-06-06 18:14:27 +0200

    modification profil
    pour la modification du profil.
    pour l'image il faut toutes les chercher dans le repertoire assets.
    Pas encore implementer la reception de la BDD

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit,Input } from '@angular/core';
+import { Component,OnInit,Input, Output } from '@angular/core';
 import { UserService } from 'src/app/service/user.service';
 import { User } from 'src/classes/User';
 import { FormGroup,Validators,FormBuilder, FormControl } from '@angular/forms';
@@ -12,36 +12,54 @@ import { Router } from '@angular/router';
 export class UserComponent implements OnInit {
   @Input() user!: User;
 
- password : string = ''
-  confirmpassword: string ='';
+  selectedFile: File |null =null;
 
+  
+
+
+
+  password : string = ''
+  confirmpassword: string ='';
+  photo:string="";
+  finalpicture :string[]=[];
   constructor(public service : UserService, private router: Router){
     this.user = this.service.getuserByIndex(0)
 
-    
-
 
   }
+
+  
 
   ngOnInit(): void {
     
   }
+
+  
+
+
+
+
  onSubmit(): void{
 
-    if(this.password == this.confirmpassword && this.password!="" && this.confirmpassword!="" &&this.user.first_name!="" && this.user.last_name!="" && this.user.email!="" && this.user.nickname!="")
+    if(this.password == this.confirmpassword && this.password!="" && this.confirmpassword!="" &&this.user.first_name!="" && this.user.last_name!="" && this.user.email!="" && this.user.nickname!="" && this.user.address.number!=0 && this.user.address.street!="" && this.user.address.city!="" && this.user.address.zip_code!="" &&this.user.picture!="" )
     {
+      this.finalpicture=this.user.picture.split("\\",3)
+        this.user.picture=this.finalpicture[2];
       this.user.password=this.password
       this.service.modifyuser(this.user);
       this.router.navigate(['/profil']);
     }
     else{
-    if (this.user.first_name!="" && this.user.last_name!="" && this.user.email!="" && this.user.nickname!="")
+    if (this.user.first_name!="" && this.user.last_name!="" && this.user.email!="" && this.user.nickname!=""&& this.user.address.number!=0 && this.user.address.street!="" && this.user.address.city!="" && this.user.address.zip_code!=""&&this.user.picture!="")
       {
+        this.finalpicture=this.user.picture.split("\\",3)
+        this.user.picture=this.finalpicture[2];
+        this.user.picture="assets/profilpictures/"+this.user.picture
         this.service.modifyuser(this.user);
         this.router.navigate(['/profil']);
 
     }
-    else{
+    else{window.alert("Vous ne respectez pas les consignes")
 
     }
   }
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
 import { Address } from 'src/classes/Address';
 import { User } from 'src/classes/User';
 
@@ -7,10 +10,49 @@ import { User } from 'src/classes/User';
   providedIn: 'root'
 })
 export class UserService {
+  public logged_user?: User;
 
   arrayuser:User[]=[];
 
-  constructor() { }
+  constructor(private http :HttpClient, private router:Router) { }
+
+
+  modifyuser(user:User):void{
+
+    const data = {
+      username:user.nickname,
+      password: user.password,
+      lastname  :user.last_name,
+      firstname:user.first_name,
+      email:user.email,
+      id:user.id,
+      streetnumber:user.address.number,
+      street:user.address.street,
+      zip:user.address.zip_code,
+      city:user.address.city,
+      adressid:user.address.id,
+      picture:user.picture
+
+
+    };
+
+    this.http.post<any>('http://localhost/src/projet/update.php', data)
+      .pipe(
+        tap(response => {
+          if (response.success == "true") {
+            if (response.user) {
+              
+                console.log("ameno")
+            }
+            this.router.navigate(['/offer-list']);
+          } else {
+            console.log("aie aie aie")
+
+          }
+        })
+      )
+      .subscribe();
+  }
 
   getUsers():User[]{
     this.arrayuser.push(new User(
@@ -20,20 +62,23 @@ export class UserService {
         "FRIDBLATT",
         "thomas.fridblatt@gmail.com",
         new Date("07/11/2017"), 
-        new Address(66,"rue du chalet","reims","51100"),
-        "testmdp")
+        new Address(1,66,"rue du chalet","reims","51100"),
+        "testmdp",
+        "assets/profilpictures/nouser.png")
         )
 
     return this.arrayuser;
   }
 
 
-  modifyuser(user: User ): void {
+  getuser2(user: User ): void {
     console.log("the new first name is ",user.first_name);
     console.log("the new last name is ",user.last_name);
     console.log("the new email is ",user.email);
     console.log("the new nickname is ",user.nickname);
-    console.log("the new mdp is",user.password)
+    console.log("the new mdp is",user.password);
+    console.log("the new adress is the",user.address.number,"of the street",user.address.street);
+    console.log("it takes place in ",user.address.city,"with a zip code of ",user.address.zip_code)
 
 
   }
@@ -42,4 +87,27 @@ export class UserService {
 
     return this.arrayuser[id]
    }
+
+
+   savePhoto(formData: FormData) {
+    return this.http.post('api/savePhoto', formData);
+  }
+
+
+  user_logged(): boolean {
+    return this.logged_user !== undefined;
+  }
+
+  // Définit les informations de l'utilisateur connecté
+  set_user(id:number,username: string, firstname: string, lastname: string, email: string, regdate: Date, address: Address,password:string,picture:string): void {
+    this.logged_user = new User(id,username, firstname, lastname, email, regdate, address,password,picture);
+  }
+
+  // Déconnecte l'utilisateur
+  logout(): void {
+    this.logged_user = undefined;
+  }
+
+
+
 }
